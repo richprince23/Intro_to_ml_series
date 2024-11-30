1dac6cf2466d718ba770171e6546da8558a2fa29
Richard Kweku Aikins <richardkns7@gmail.com>
2024-11-30 16:28:25 +0000

    added doc string

--- linear_reg/main.py
+++ linear_reg/main.py
@@ -5,37 +5,45 @@ import csv
 from sklearn.linear_model import LinearRegression
 
 
+""" 
+
+We want to predict a car's fuel efficiency in miles per gallon based on how heavy the car is, and we have the following dataset:
+
+view data.csv file
+"""
+
+
+# read data file using csv
 with open('data.csv', mode='r') as file:
     # Step 2: Create a CSV reader object
     csv_reader = csv.reader(file)
 
     next(csv_reader)
 
+    # save data to array
     weight = []
     gallon  = []
 
     # Step 3: Iterate through the rows
     for row in csv_reader:
-        weight.append([float( row[0])])
-        gallon.append(float( row[1]))
 
-# weight = np.array(weight).reshape(-1, 1)
-# gallon = np.array(gallon)
+        weight.append([float(row[0])]) # use 2d array for the feature - weight of vehicle
+        gallon.append(float(row[1]))
 
 
 # create Linear Regression model
 model = LinearRegression()
 
-
 # train model
 model.fit(weight, gallon)
 
-# new_weight = np.array([3.22]).reshape(1, -1)
 
+# print gradient 
 print(f"Model Coefficient (Slope): {model.coef_[0]:.2f}")
 print(f"Model Intercept: {model.intercept_:.2f}")
 
 # new prediction
-
 new_res = model.predict([[3.22]])
+
+# print predicted value
 print(new_res)
